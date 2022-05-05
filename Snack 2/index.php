6eddda4735ddb496f3a0cdc5a745e39dab75ecea
Andrea Pontillo <andreapontillo97@gmail.com>
2022-05-05 17:57:43 +0200

    stesura snack 1 e 2

---
+++ Snack 2/index.php
@@ -0,0 +1,49 @@
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php 
+ 
+$name = ( isset($_GET['name']) ) ? $_GET['name'] : '' ;
+$mail = ( isset($_GET['mail']) ) ? $_GET['mail'] : '' ;
+$age = ( isset($_GET['mail']) ) ? $_GET['mail'] : '' ;
+
+$age = intval ($age);
+
+$accessGranted = "Accesso riuscito";
+$accessDenied = "Accesso negato";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form method="GET" style="margin-top: 10px">
+            <label for="name">Name:</label>
+            <input type="text" name="name">
+
+            <label for="mail">Mail:</label>
+            <input type="text" name="mail">
+
+            <label for="age">Age:</label>
+            <input type="text" name="age">
+            <button type="submit">Invia</button>
+        </form>
+    </div>
+    <div>
+        <?php 
+         if(strlen($name) > 3 && strpos($mail, '@') == true && strpos($mail, '.') == true && is_int($age) == true && $age !== 0){
+            echo $accessGranted;
+        } else {
+            echo $accessDenied;
+        };
+        ?>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Snack 7/index.php
@@ -0,0 +1,47 @@
+<!-- Creare un array contenente qualche alunno di un’ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php 
+
+$alunni = [
+    [
+        "nome" => "Paolo",
+        "cognome" => "Rossi",
+        "voti" => [
+            9 , 8 , 5 , 6
+        ]
+        ],
+    [
+        "nome" => "Mario",
+        "cognome" => "Biondi",
+        "voti" => [
+            4 , 5 , 5 , 2
+        ]
+        ],
+    [
+        "nome" => "Francesco",
+        "cognome" => "Esposito",
+        "voti" => [
+            7 , 6 , 5 , 6
+        ]
+    ]
+        ];
+
+var_dump ($alunni);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+</body>
+</html>
